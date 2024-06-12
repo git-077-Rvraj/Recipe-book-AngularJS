abd3404fdfa76709d018a0be6761f5092911109e
Raj <raj.ravi@siemens.com>
2024-06-12 19:19:05 +0530

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { Component, NgModule } from "@angular/core";
+import { Route, RouterModule } from "@angular/router";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipesStartComponent } from "./recipes/recipes-start/recipes-start.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
+
+const appRoutes = [
+    {path: '', redirectTo:'/recipes', pathMatch: 'full'},
+    {path: 'recipes', component:RecipesComponent, children:[
+        {path: '', component:RecipesStartComponent},
+        {path: 'new', component:RecipeEditComponent},
+        {path: ':id', component:RecipeDetailComponent},
+        {path: ':id/edit', component:RecipeEditComponent}
+    ]},
+    {path: 'shopping-list', component:ShoppingListComponent}
+]  
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{
+
+}
+
